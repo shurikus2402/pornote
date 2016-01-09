29143442f48dc795f38155ee062276040a10698f
Thibault iTech <tallancon@gmail.com>
2016-01-09 11:08:36 +0100

    Added database config + Few changes

---
+++ pornote/models.py
@@ -0,0 +1,19 @@
+from pornote import db
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    email = db.Column(db.String(128), index=True, unique=True)
+    class_nb = db.Column(db.Integer)
+
+    homework = db.relationship("Homework", backref="author")
+
+class Homework(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    member_id = db.Column(db.Integer, db.ForeignKey("member.id"), nullable=False)
+
+    name = db.Column(db.String(100))
+    time_end = db.Column(db.DateTime)
